218adbc50ebd504b9365106c1a02805fd2d2173c
ChHarshini08 <harshi2004harpal@gmail.com>
2025-01-04 07:34:36 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,43 @@
 def hello_func():
-    pass
+   print('Hello All!!')
+hello_func()
 
-print(hello_func())
+
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+courses=['Math', 'Art']
+info ={'Name': 'John', 'Age': '22'}
+
+student_info(*courses, **info)
+
+
+month_days = [0,31,30,31,28,30,31,30,30,31,31,30,30]
+def is_leap(year):
+     return year%4==0 and (year%100!=0 or year%400==0)
+def days_in_month(year, month):
+     if not 1<= month <=12:
+         return 'Invalid'
+     if month == 2 and is_leap(year):
+         return 29
+     return month_days[month]
+print(is_leap(2017))
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
